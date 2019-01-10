bfab5e36468109b0df0fea10caf744e5713c70bb
DevisMevis <stommeldavid@gmail.com>
2019-01-10 09:28:13 +0100

    new

--- src/main.java
+++ src/main.java
@@ -1,8 +1,11 @@
+import javax.swing.*;
 
 public class main {
-
+    static view Fenster;
+    
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		view.run();
+		
 
 	}
 
---
+++ src/view.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+public class view {
+
+	
+    public static void run() {
+    	JFrame fenster = new JFrame("Hallo Welt mit Swing");
+        fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel("123gdhn6hqwgthqw42u4");
+        
+   
+        fenster.setSize(1920,1800);
+        fenster.getContentPane().setBackground( Color.black );
+        fenster.setVisible(true);
+        
+        
+    }
+
+}
